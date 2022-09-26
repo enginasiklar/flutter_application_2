3ba6f6cff2973969b7091c299523ec7b008bf8e6
Seyfülmülük Kutluk <seyfulmulukkutluk@Seyfulmuluks-MacBook-Pro.local>
2022-09-26 21:24:35 +0300

    [Seyfulmuluk Kutluk][api listening refactoring]

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,5 +1,4 @@
-import 'dart:developer';
-
+// ignore: import_of_legacy_library_into_null_safe
 import 'package:http/http.dart' as http;
 import 'package:flutter_application_2/constants.dart';
 import 'package:flutter_application_2/model/user_model.dart';
