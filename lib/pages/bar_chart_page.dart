f42916d10de0a523346f2374bfe9a7a8ad1d9cc7
enginasiklar <enginasiklar77@gmail.com>
2022-09-27 13:39:34 +0300

    null safety changes

--- lib/pages/bar_chart_page.dart
+++ lib/pages/bar_chart_page.dart
@@ -4,21 +4,30 @@ import 'package:flutter_application_2/charts/bar_chart.dart';
 
 class BarChartPage extends StatelessWidget {
   const BarChartPage({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-      child: Center(
-        child: Column(
-          children: [
-            Container(
-              margin: const EdgeInsets.all(10),
-              child: BarChartWidget(pricePoints),
-            ),
-          ],
-        ),
+      body: SingleChildScrollView(
+        child: Center(
+            child: FutureBuilder<List<PricePoint>>(
+                future: getPricePoints(),
+                builder: (
+                  BuildContext context,
+                  AsyncSnapshot snapshot,
+                ) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [CircularProgressIndicator()]);
+                  }
+                  return Column(children: [
+                    Container(
+                        margin: const EdgeInsets.all(10),
+                        child: BarChartWidget(snapshot.data))
+                  ]);
+                })),
       ),
-    ));
+    );
   }
 }
--- lib/pages/line_chart_page.dart
+++ lib/pages/line_chart_page.dart
@@ -1,39 +1,35 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_application_2/charts/line_chart.dart';
 import 'package:flutter_application_2/price_points.dart';
 
 class LineChartPage extends StatelessWidget {
   const LineChartPage({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
         child: Center(
-          child: Column(
-            children: [
-              Container(
-                margin: const EdgeInsets.all(10),
-                child: LineChartWidget(pricePoints),
-              ),
-              /*ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (BuildContext context) {
-                        return const LearnFlutter();
-                      },
-                    ),
-                  );
-                },
-                child: const Text('learn flutter'),
-              ),*/
-            ],
-          ),
-        ),
+            child: FutureBuilder<List<PricePoint>>(
+                future: getPricePoints(),
+                builder: (
+                  BuildContext context,
+                  AsyncSnapshot snapshot,
+                ) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [CircularProgressIndicator()]);
+                  }
+                    return Column(children: [
+                      Container(
+                          margin: const EdgeInsets.all(10),
+                          child: LineChartWidget(snapshot.data))
+                    ]);
+
+                })),
       ),
     );
   }
 }
+
