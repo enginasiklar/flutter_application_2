b26930056ef53f8d124cf66616194ce4291e6ee3
fadymena <fadymena1999@gmail.com>
2023-02-06 19:11:41 +0100

    UI fixes

--- lib/pages/predict_line_chart_page.dart
+++ lib/pages/predict_line_chart_page.dart
@@ -3,36 +3,50 @@ import '../charts/predict_line_chart.dart';
 import '../model/line_model.dart';
 import '../services/api_service.dart';
 
-class PredictLineDataPage extends StatelessWidget {
+class PredictLineDataPage extends StatefulWidget {
   const PredictLineDataPage({super.key});
+
+  @override
+  State<PredictLineDataPage> createState() => _PredictLineDataPageState();
+}
+
+class _PredictLineDataPageState extends State<PredictLineDataPage> {
+  bool closeViewed = false;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Center(
-            child: Padding(
-          padding: const EdgeInsets.all(15),
-          child: FutureBuilder<List<LineData>>(
-              future: ApiService().getLineData(),
-              builder: (
-                BuildContext context,
-                AsyncSnapshot snapshot,
-              ) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [CircularProgressIndicator()]);
-                }
-                return Column(children: [
-                  PredictLineDataWidget(
-                      points: snapshot.data, showCloseData: true),
-                  const SizedBox(height: 10),
-                  PredictLineDataWidget(
-                      points: snapshot.data, showCloseData: false),
-                ]);
-              }),
-        )),
+    return SingleChildScrollView(
+      padding: const EdgeInsets.all(15),
+      child: Center(
+        child: FutureBuilder<List<LineData>>(
+            future: ApiService().getLineData(),
+            builder: (
+              BuildContext context,
+              AsyncSnapshot snapshot,
+            ) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const CircularProgressIndicator();
+              }
+              return Column(children: [
+                closeViewed
+                    ? PredictLineDataWidget(
+                        points: snapshot.data, showCloseData: true)
+                    : PredictLineDataWidget(
+                        points: snapshot.data, showCloseData: false),
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text("Show close price"),
+                    Switch(
+                      value: closeViewed,
+                      onChanged: (value) => setState(() {
+                        closeViewed = !closeViewed;
+                      }),
+                    ),
+                  ],
+                ),
+              ]);
+            }),
       ),
     );
   }
