54ca1a0eba324da32be35f5cc01ad51c83580879
enginasiklar <enginasiklar77@gmail.com>
2023-07-17 13:43:55 +0300

    non functional simulation page

--- lib/pages/stock_page.dart
+++ lib/pages/stock_page.dart
@@ -2,6 +2,7 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_2/model/predictions_short_data.dart';
+import 'package:flutter_application_2/pages/simulation_page.dart';
 import 'package:flutter_application_2/pages/stock_data_page.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 import '../model/stock_model.dart';
@@ -106,6 +107,19 @@ class _StockPageState extends State<StockPage> {
       appBar: AppBar(
         title: Text(widget.stockName),
         actions: [
+          IconButton(
+            onPressed: () {
+              // Navigate to the simulation input page when the button is pressed
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (BuildContext context) => SimulationInputPage(),
+                ),
+              );
+            },
+            tooltip: 'Simulation',
+            icon: const Icon(Icons.calculate),
+          ),
           IconButton(
             onPressed: () {
               Navigator.of(context).push(
@@ -449,7 +463,6 @@ class _StockPageState extends State<StockPage> {
     );
   }
 
-
   Widget datesSetBar() {
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 20),
@@ -599,4 +612,7 @@ class _StockPageState extends State<StockPage> {
       ],
     );
   }
+
+
+
 }
